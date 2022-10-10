142367023a6b73b1c419a52baf2e23ef90f98b32
AhlaamK-tap <a.kazi@tap.company>
2022-10-10 17:27:09 +0300

    commit ui changes

--- app/src/main/java/company/tap/cardsdk/MainActivity.kt
+++ app/src/main/java/company/tap/cardsdk/MainActivity.kt
@@ -2,10 +2,16 @@ package company.tap.cardsdk
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import company.tap.taplocalizationkit.LocalizationManager
+import company.tap.tapuilibrary.themekit.ThemeManager
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        ThemeManager.loadTapTheme(this.resources,R.raw.defaultlighttheme,"lighttheme")
+        LocalizationManager.loadTapLocale(this.resources,R.raw.lang)
+        LocalizationManager.setLocale(this, Locale("en"))
     }
 }
